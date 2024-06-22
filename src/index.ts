626afeb990d4e1d142588665e5e409300675e7ff
marshmallow <160699831+marshmallowscc@users.noreply.github.com>
2024-06-23 00:32:03 +0900

    予定削除機能を実装

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,83 @@ const SHEET_URL: any = PropertiesService.getScriptProperties().getProperty("SHEE
 const calendar = CalendarApp.getCalendarById(CAL_ID)
 const spreadSheet = SpreadsheetApp.openByUrl(SHEET_URL)
 const sheet = spreadSheet.getSheetByName('授業選択')
-const weekStart: Date = week_start()
 
 function main() {
   
 }
 
-function week_start(){
-  let today = new Date();
-  let this_year = today.getFullYear();
-  let this_month = today.getMonth();
-  let date = today.getDate();
-  let day_num = today.getDay();
-  let this_monday = date - day_num + 1
+function createEvent() {
+  
+}
 
-  //月曜日の年月日
-  let start_date = new Date(this_year, this_month, this_monday);
-  return start_date
+// function addEvent() {
+//   const listLastRow = 9;
+//   // console.log(listLastRow)
+//   let listCol = 4
+
+//   for(listCol = 4; listCol < 7; listCol++) {
+//     console.log(listCol)
+
+//     for(let i = 1; i < listLastRow; i++) {
+//       // console.log(i);
+//       // タイトル
+//       const title = sheet.getRange(i + 1, listCol).getValue()
+//       // titleが登録されていない場合、この行をパスする
+//       if(title == '') {
+//         continue
+//       }
+//       console.log(title)
+//       const day = sheet.getRange(1, listCol).getValue().getDay();
+//       const days = [CalendarApp.Weekday.SANDAY, CalendarApp.Weekday.MONDAY, CalendarApp.Weekday.TUESDAY, CalendarApp.Weekday.WEDNESDAY, CalendarApp.Weekday.THURSDAY, CalendarApp.Weekday.FRIDAY, CalendarApp.Weekday.SATURDAY];
+      
+//       const ssDate = sheet.getRange(1, listCol).getValue().getDate();
+//       console.log(ssDate)
+//       // 開始時間
+//       const startHours = sheet.getRange(i + 1, 2).getValue().getHours()
+//       const startMinutes = sheet.getRange(i + 1, 2).getValue().getMinutes()
+//       let startTime = new Date(sheet.getRange(2, 7).getValue())
+//       startTime = new Date(startTime.setDate(ssDate))
+//       startTime = startTime.setHours(startHours, startMinutes, 0)
+//       startTime = new Date(startTime)
+//       console.log(startTime)
+
+//       // 終了時間
+//       const endHours = sheet.getRange(i + 1, 3).getValue().getHours()
+//       const endMinutes = sheet.getRange(i + 1, 3).getValue().getMinutes()
+//       let endTime = new Date(sheet.getRange(2, 7).getValue())
+//       endTime = new Date(endTime.setDate(ssDate))
+//       endTime = endTime.setHours(endHours, endMinutes, 0)
+//       endTime = new Date(endTime)
+//       console.log(endTime)
+      
+//       let recurrence = CalendarApp.newRecurrence()
+//       .addWeeklyRule().onlyOnWeekday(days[day]).times(12);
+//       calendar.createEventSeries(title, startTime, endTime, recurrence)
+//     }
+//   }
+
+  
+// }
+
+
+function next_monday(){
+  let now = new Date();
+  let y = now.getFullYear();
+  let m = now.getMonth();
+  //曜日の番号を取得
+  let day_num = now.getDay();
+  let d = now.getDate() - day_num + 1;
+
+  return new Date(y, m, d);
 }
 
 
 function deleteEvent() {
-  const delEvents = calendar.getEvents(weekStart, new Date(weekStart.setDate(weekStart.getDate() + 90)))
-  console.log(delEvents[0])
+  const delEvents = calendar.getEvents(next_monday(), new Date(next_monday().setDate(next_monday().getDate() + 90)))
+  console.log(delEvents.length)
+  for (let i = 0; i < delEvents.length; i++) {
+    let delEvent = delEvents[i];
+    delEvent.deleteEvent();
+    console.log('削除しました')
+  }
 }
-
-
-// function deleteEvent() {
-//   const delStart = sheet.getRange(2 ,7).getValue();
-//   console.log(delStart)
-//   const delEvents = calendar.getEvents(delStart, new Date('2025-04-19'));
-//   console.log(delEvents.length)
-//   for(let i = 0; i < delEvents.length; i++) {
-//     let delEvent = delEvents[i];
-//     delEvent.deleteEvent();
-//     // console.log('削除しました')
-//   }
-// }
